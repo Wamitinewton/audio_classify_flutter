9b529dec851a6b40130dd58aa4c4734875bcdec1
Wamitinewton <wamitinewton@gmail.com>
2024-07-21 22:24:57 +0300

    running the inference

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:typed_data';
+
 import 'package:audio_classify_flutter/audio_classification_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -25,7 +29,7 @@ class AudioClassificationApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.title});
 
   final String title;
 
@@ -44,9 +48,127 @@ class _HomeScreenState extends State<HomeScreen> {
 
   late AudioClassificationHelper _helper;
   List<MapEntry<String, double>> _classification = List.empty();
+  final List<Color> _primaryProgressColorList = [
+     const Color(0xFFF44336),
+    const Color(0xFFE91E63),
+    const Color(0xFF9C27B0),
+    const Color(0xFF3F51B5),
+    const Color(0xFF2196F3),
+    const Color(0xFF00BCD4),
+    const Color(0xFF009688),
+    const Color(0xFF4CAF50),
+    const Color(0xFFFFEB3B),
+    const Color(0xFFFFC107),
+    const Color(0xFFFF9800)
+  ];
+
+  final List<Color> _backgroundProgressColorList = [
+     const Color(0x44F44336),
+    const Color(0x44E91E63),
+    const Color(0x449C27B0),
+    const Color(0x443F51B5),
+    const Color(0x442196F3),
+    const Color(0x4400BCD4),
+    const Color(0x44009688),
+    const Color(0x444CAF50),
+    const Color(0x44FFEB3B),
+    const Color(0x44FFC107),
+    const Color(0x44FF9800)
+  ];
+  var _showError = false;
+
+  void _startRecorder() {
+    try {
+      platform.invokeMethod('startRecord');
+    } on PlatformException catch(e){
+      log("Failed to start record: '${e.message}'. ");
+    }
+  }
+
+  Future<bool> _requestPermission() async {
+    try {
+      return await platform.invokeMethod('requestPermissionAndCreateRecorder', {
+        "sampleRate": _sampleRate,
+        "requiredInputBuffer": _requiredInputBuffer,
+      });
+    } on Exception catch(e){
+      log("Failed to create recorder: '${e.toString()}'.");
+      return false;
+    }
+  }
+
+  Future<Float32List> _getAudioFloatArray() async {
+    var audioFloatArray = Float32List(0);
+    try {
+      final Float32List result = 
+      await platform.invokeMethod('getAudioFloatArray');
+      audioFloatArray = result;
+    } on PlatformException catch(e) {
+      log("Failed to get audio array: '${e.message}'.");
+    }
+    return audioFloatArray;
+  }
+
+  Future<void> _closeRecorder() async {
+    try {
+      await platform.invokeMethod('closeRecorder');
+      _helper.closeInterpreter();
+    } on PlatformException {
+      log("Failed to close recorder");
+    }
+  }
+
+  @override
+   initState() {
+    // calling the initRecoder method here
+    _initRecorder();
+    super.initState();
+  }
+
+  Future<void> _initRecorder() async {
+    _helper = AudioClassificationHelper();
+    await _helper.initHelper();
+    bool sucess = await _requestPermission();
+    if (sucess) {
+      _startRecorder();
+
+      Timer.periodic(const Duration(milliseconds: _expectAudioLength), (timer) {
+        // running the inference here
+      });
+    } else{
+      setState(() {
+        _showError = true;
+      });
+    }
+  }
+
+  Future<void> _runInference() async {
+    Float32List inputArray = await _getAudioFloatArray();
+    final result = 
+    await _helper.inference(inputArray.sublist(0, _requiredInputBuffer));
+    setState(() {
+      // taking top 3 classifcations
+      _classification = (result.entries.toList()
+      ..sort(
+        (a, b) => a.value.compareTo(b.value),
+      )
+      )
+      .reversed
+      .take(3)
+      .toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    _closeRecorder();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
+
+  
 }
 
