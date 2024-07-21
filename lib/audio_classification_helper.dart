ec0841e4a568d589ae6106332da71fa111dde6dd
Wamitinewton <wamitinewton@gmail.com>
2024-07-21 19:08:25 +0300

    calculating buffer size and using map entry

--- lib/audio_classification_helper.dart
+++ lib/audio_classification_helper.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'dart:developer';
 
@@ -26,5 +29,34 @@ class AudioClassificationHelper {
     log(_outputTensor.shape.toString());
     log('interpreter loaded succesfully');
     
+  }
+
+  // load labels from the assets
+
+  Future<void> _loadLabels() async {
+    final labelTxt = await rootBundle.loadString(_labelsPath);
+    _labels = labelTxt.split('\n');
+  }
+
+  Future<void> initHelper() async {
+    await _loadLabels();
+    await _loadModel();
+  }
+
+  Future<Map<String, double>> inference(Float32List input) async {
+    final output = [List<double>.filled(521, 0.0)];
+    _interpreter.run(List.of(input), output);
+    var classification = <String, double>{};
+
+    for (var i = 0; i < output[0].length; i++) {
+      
+      classification[_labels[i]] = output[0][i];
+    }
+
+    return classification;
+  }
+
+  closeInterpreter() {
+    _interpreter.close();
   }
 }
